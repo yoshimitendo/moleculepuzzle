e640f448719d3c5a68a8193764ff7eb8a98cb6c6
yoshimitendo <vk6sgysw7p@privaterelay.appleid.com>
2026-08-31 23:34:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,78 +21,94 @@ function resizeGame() {
 window.addEventListener("resize", resizeGame);
 const GAME_SCALE = resizeGame();
 
-const SELECT_HEIGHT = 1000;
+const SELECT_HEIGHT = 1300;
 
 const ELEMENTS = [
-    {text: "H", color: "#4987ae"},
-    {text: "C", color: "#2c8a5d"},
-    {text: "O", color: "#9d333e"}
+    {text: "H", color: "#4987ae", arm: 1},
+    {text: "C", color: "#2c8a5d", arm: 4},
+    {text: "N", color: "#9f6035", arm: 3},
+    {text: "O", color: "#9d333e", arm: 2}
 ];
 
-for (let i = 0; i < 4; i++) {
-    const piece= document.createElement("div");
-    piece.className = "pieces";
-    piece.element = Math.floor(Math.random() * ELEMENTS.length);
-    gameLayer.appendChild(piece);
+const selectElements = [];
+const buildElements = [];
 
-    piece.textContent = ELEMENTS[piece.element].text;
-    piece.style.background = `
-        radial-gradient(
-            circle,
-            ${ELEMENTS[piece.element].color + "00"} 0%,
-            ${ELEMENTS[piece.element].color + "8c"} 100%)
-    `;
-    piece.style.border = 
-        `2px solid ${ELEMENTS[piece.element].color}`
-    piece.X = i * 200;
-    piece.Y = SELECT_HEIGHT;
-    piece.style.left = `${piece.X}px`
-    piece.style.top = `${piece.Y}px`
+function creatSelect() {
+    for (let i = 0; i < 4; i++) {
+        const piece= document.createElement("div");
+        piece.className = "pieces";
+        piece.element = Math.floor(Math.random() * ELEMENTS.length);
+        gameLayer.appendChild(piece);
+        selectElements.push(piece);
 
+        piece.textContent = ELEMENTS[piece.element].text;
+        piece.style.background = `
+            radial-gradient(
+                circle,
+                ${ELEMENTS[piece.element].color + "22"} 0%,
+                ${ELEMENTS[piece.element].color + "99"} 100%)
+        `;
+        piece.style.border = 
+            `3px solid ${ELEMENTS[piece.element].color}`
+        piece.X = i * 200;
+        piece.Y = SELECT_HEIGHT;
+        piece.style.left = `${piece.X}px`
+        piece.style.top = `${piece.Y}px`
+        piece.style.scale = "0";
+        piece.scale = 0.6;
+        piece.isAnimating = false;
+        setTimeout(() => {
+            soft(piece);
+        }, i * 40);
 
-    piece.style.scale = "0";
-    piece.scale = 0;
-    piece.isAnimating = false;
-    setTimeout(() => {
-        soft(piece);
-    }, i * 40);
+        piece.addEventListener("pointerdown", (e) => {
+            e.preventDefault();
+            piece.isPointer = true;
+            piece.style.filter = "brightness(1.5)";
+            piece.style.zIndex = "1";
+            piece.oldX = piece.X;
+            piece.oldY = piece.Y;
+            piece.pointerX = e.clientX;
+            piece.pointerY = e.clientY;
 
-    piece.addEventListener("pointerdown", (e) => {
-        e.preventDefault();
-        piece.isPointer = true;
-        piece.style.filter = "brightness(1.5)";
-        piece.style.zIndex = "1";
-        piece.oldX = piece.X;
-        piece.oldY = piece.Y;
-        piece.pointerX = e.clientX;
-        piece.pointerY = e.clientY;
+            piece.scale = 1.2;
+            piece.scaleSpeed = 0;
+            soft(piece);
+            
+            piece.setPointerCapture(e.pointerId);
+        })
 
-        piece.scale = 1.5;
-        piece.scaleSpeed = 0;
-        soft(piece);
-        
-        piece.setPointerCapture(e.pointerId);
-    })
+        piece.addEventListener("pointerup", (e) => {
+            e.preventDefault();
+            if (!piece.isPointer) return;
+            piece.style.filter = "brightness(1.0)";
+            piece.style.zIndex = "0";
+            piece.isPointer = false;
 
-    piece.addEventListener("pointerup", (e) => {
-        e.preventDefault();
-        if (!piece.isPointer) return;
-        piece.style.filter = "brightness(1.0)";
-        piece.style.zIndex = "0";
-        piece.isPointer = false;
+            piece.releasePointerCapture(e.pointerId);
 
-        piece.releasePointerCapture(e.pointerId);
-    })
+            const index = selectElements.findIndex(i => i === piece);
+            selectElements.splice(index,1);
+            buildElements.push(piece);
+            for (let i = 0; i < 3; i++) {
+                selectElements[0].remove();
+                selectElements.splice(0,1);
+            }
+            creatSelect();
+        })
 
-    piece.addEventListener("pointermove", (e) => {
-        e.preventDefault();
-        if (!piece.isPointer) return;
-        piece.X = (e.clientX - piece.pointerX) / GAME_SCALE + piece.oldX;
-        piece.Y = (e.clientY - piece.pointerY) / GAME_SCALE + piece.oldY;
-        piece.style.left = `${piece.X}px`;
-        piece.style.top = `${piece.Y}px`;
-    })
+        piece.addEventListener("pointermove", (e) => {
+            e.preventDefault();
+            if (!piece.isPointer) return;
+            piece.X = (e.clientX - piece.pointerX) / GAME_SCALE + piece.oldX;
+            piece.Y = (e.clientY - piece.pointerY) / GAME_SCALE + piece.oldY;
+            piece.style.left = `${piece.X}px`;
+            piece.style.top = `${piece.Y}px`;
+        })
+    }
 }
+
+creatSelect();
 
 function soft(p) {
     if (p.isAnimating) return;
