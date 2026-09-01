623a5996a39b36465511e4b36abb64e8fb294300
yoshimitendo <vk6sgysw7p@privaterelay.appleid.com>
2026-09-01 20:36:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -31,11 +31,17 @@ boardLayer.style.width = `${GRID_COLS * (GRID_SIZE + GRID_GAP) - GRID_GAP}px`;
 boardLayer.style.height = `${GRID_ROWS * (GRID_SIZE + GRID_GAP) - GRID_GAP}px`;
 
 const ELEMENTS = [
-    {text: "H", color: "#4987ae", arm: 1},
-    {text: "C", color: "#2c8a5d", arm: 4},
-    {text: "N", color: "#9f6035", arm: 3},
-    {text: "O", color: "#9d333e", arm: 2}
+    {text: "H", col: "#4987ae", arm: 1},
+    {text: "C", col: "#2c8a5d", arm: 4},
+    {text: "N", col: "#9f6035", arm: 3},
+    {text: "O", col: "#9d333e", arm: 2}
 ];
+
+const MOLECULES = [
+    {text: "H₂", con: ["H", "H"]},
+    {text: "N₂", con: ["N", "N"]},
+    {text: "O₂", con: ["O", "O"]},
+]
 
 const selectPiece = [];
 
@@ -49,12 +55,12 @@ function creatSelect(i, j, d) {
     piece.style.background = `
         radial-gradient(
             circle,
-            ${ELEMENTS[piece.element].color + "22"} 0%,
-            ${ELEMENTS[piece.element].color + "99"} 100%
+            ${ELEMENTS[piece.element].col + "22"} 0%,
+            ${ELEMENTS[piece.element].col + "99"} 100%
         )
     `;
     piece.style.border = 
-        `3px solid ${ELEMENTS[piece.element].color}`;
+        `3px solid ${ELEMENTS[piece.element].col}`;
     piece.style.width = `${GRID_SIZE}px`;
     piece.style.height = `${GRID_SIZE}px`;
     piece.GridX = i;
@@ -109,9 +115,13 @@ document.addEventListener("pointerup", (e) => {
     selectPiece.forEach((e) => {
         e.style.filter = "brightness(1)";
         e.style.border = 
-        `3px solid ${ELEMENTS[e.element].color}`;
+        `3px solid ${ELEMENTS[e.element].col}`;
     })
     selectPiece.length = 0;
+
+    document.querySelectorAll(".lines").forEach(element => {
+        element.remove();
+    })
 
     isPointer = false;
 })
@@ -126,26 +136,62 @@ document.addEventListener("pointermove", (e) => {
             const last = selectPiece[selectPiece.length - 1]
             last.style.filter = "brightness(1)";
             last.style.border = 
-                `3px solid ${ELEMENTS[last.element].color}`;
+                `3px solid ${ELEMENTS[last.element].col}`;
             selectPiece.splice(selectPiece.length - 1, 1)
+            document.querySelectorAll(".lines").forEach(element => {
+                element.remove();
+            })
+            genLine();
         }
         return;
     }
     const gapX = Math.abs(target.GridX - selectPiece[selectPiece.length - 1].GridX);
     const gapY = Math.abs(target.GridY - selectPiece[selectPiece.length - 1].GridY);
     if (gapX + gapY !== 1) return;
+    document.querySelectorAll(".lines").forEach(element => {
+        element.remove();
+    })
     addPiece(target);
+    genLine();
 
 })
 
 function addPiece(t) {
     t.style.filter = "brightness(1.6)";
     t.style.border = 
-        `5px solid ${ELEMENTS[t.element].color}`;
+        `5px solid ${ELEMENTS[t.element].col}`;
 
-    t.scale = 1.4;
+    t.scale = 1.3;
     t.scaleSpeed = 0;
     soft(t);
 
     selectPiece.push(t);
+}
+
+function genLine() {
+    for (let i = 1; i < selectPiece.length; i++) {
+        createLine(selectPiece[i].GridX, selectPiece[i].GridY, selectPiece[i - 1].GridX, selectPiece[i - 1].GridY)
+    }
+}
+
+function createLine(x, y, x2, y2) {
+    const dx = x2 - x;
+    const dy = y2 - y;
+
+    const line = document.createElement("div");
+    line.className = "lines";
+    line.style.zIndex = -1;
+    boardLayer.appendChild(line);
+
+    if (dx === 0) {
+        line.style.width = `12px`;
+        line.style.height = `${GRID_GAP + 40}px`;
+    } else {
+        line.style.width = `${GRID_GAP + 40}px`;
+        line.style.height = `12px`;
+    }
+    line.style.left = 
+        `${x * (GRID_SIZE + GRID_GAP) + GRID_SIZE / 2 + dx * (GRID_SIZE + GRID_GAP) / 2}px`;
+    line.style.top = 
+        `${y * (GRID_SIZE + GRID_GAP) + GRID_SIZE / 2 + dy * (GRID_SIZE + GRID_GAP) / 2}px`;
 }
